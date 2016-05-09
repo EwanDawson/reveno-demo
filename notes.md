2dd9c986ff0258341c7b1a01a78356a8f90a5bca
Ewan Dawson <ewan@lazygun.net>
2016-05-09 07:53:44 +0100

    Generalised entity update command Updating across different branches now working

--- notes.md
+++ notes.md
@@ -1,3 +1,5 @@
 * You may only use a single view mapping per domain class
 * Event handling doesn't happen inside the transaction. Sync events will be processed in the order in which they
   are fired, but they are only handled after the transaction has committed.
+* _Does `conditionalCommand` run before of after the transaction handler? Can we specify post-conditions on the
+  transaction?_
--- src/main/kotlin/net/lazygun/experiment/reveno/demo/kotlin/Account.kt
+++ src/main/kotlin/net/lazygun/experiment/reveno/demo/kotlin/Account.kt
@@ -3,21 +3,23 @@ package net.lazygun.experiment.reveno.demo.kotlin
 import org.reveno.atp.api.Reveno
 import org.reveno.atp.api.RevenoManager
 
-data class Account private constructor (val name: String, val balance: Int) {
-    constructor(name: String) : this (name, 0)
+data class Account private constructor (val name: String, val balance: Int, val childAccounts: List<String>) {
+    constructor(name: String) : this (name, 0, listOf())
     operator fun plus(amount: Int) : Account = copy(balance = balance + amount)
+    operator fun minus(amount: Int) : Account = copy(balance = balance - amount)
+    fun addChild(child: Entity) : Account = copy(childAccounts = childAccounts + child.entity.identifier)
     data class Entity private constructor(val entity: VersionedEntity<Account>) {
         constructor(account: Account) : this(VersionedEntity(account, type))
         fun update(mutator: (Account) -> Account) : Entity = Entity(entity.update(mutator))
         fun delete() : Entity = Entity(entity.delete())
     }
-    data class View(val id: Long, val identifier: String, val name: String, val balance: Int, val version: Long, val deleted: Boolean)
+    data class View(val id: Long, val identifier: String, val name: String, val balance: Int, val version: Long, val deleted: Boolean, val childAccounts: List<String>)
     companion object {
         val type = "Account"
         val domain = Entity::class.java
         val view = View::class.java
         fun map(reveno: RevenoManager) { reveno.viewMapper(domain, view) { id, e, r ->
-            View(id, e.entity.identifier, e.entity.value.name, e.entity.value.balance, e.entity.version, e.entity.deleted)
+            View(id, e.entity.identifier, e.entity.value.name, e.entity.value.balance, e.entity.version, e.entity.deleted, e.entity.value.childAccounts)
         }}
     }
 }
@@ -44,7 +46,7 @@ internal fun initAccountDomain(reveno: Reveno) {
             val entityChangedEvent = EntityChangedEvent(after.entity, beforeId, afterId)
             entityChange(txn.id(EntityChange.domain), entityChangedEvent, ctx.repo())
             ctx.eventBus().publishEvent(entityChangedEvent)
-            println("Changed balance of account ${before.entity.value.name} from ${before.entity.value.balance} to ${after.entity.value.balance}")
+            println("Updated account ${before.entity.identifier} from ${before.entity.value} to ${after.entity.value}")
         }
         .uniqueIdFor(Account.domain, EntityChange.domain)
         .conditionalCommand { command, context ->
@@ -61,7 +63,7 @@ internal fun initAccountDomain(reveno: Reveno) {
             val entityChangedEvent = EntityChangedEvent(after.entity, beforeId, afterId)
             entityChange(txn.id(EntityChange.domain), entityChangedEvent, ctx.repo())
             ctx.eventBus().publishEvent(entityChangedEvent)
-            println("Deleted $after")
+            println("Deleted ${after.entity}")
         }
         .uniqueIdFor(Account.domain, EntityChange.domain)
         .conditionalCommand { command, context ->
